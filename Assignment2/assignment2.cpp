56956574006eeb692234dfb9819761017043b692
Shreyas <shreyasbhatia09@gmail.com>
2017-09-13 23:39:47 -0400

    Adding initial cpp file and image to repo

---
+++ Assignment2/assignment2.cpp
@@ -0,0 +1,10 @@
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2\imgproc\imgproc.hpp>
+
+using namespace std;
+
+int main()
+{
+	cv::Mat image = cv::imread("greyscale.jpg", CV_LOAD_IMAGE_COLOR);
+
+}

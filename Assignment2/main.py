ae994f1bfc22f8b713faccf1738d4731f1e4a3ec
Shreyas <shreyasbhatia09@gmail.com>
2017-09-17 22:28:05 -0400

    Low pass Filter completed

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -7,25 +7,31 @@ import os
 import sys
 import cv2
 import numpy
+from matplotlib import pyplot
+
 
 def help_message():
-   print("Usage: [Question_Number] [Input_Options] [Output_Options]")
-   print("[Question Number]")
-   print("1 Histogram equalization")
-   print("2 Frequency domain filtering")
-   print("3 Laplacian pyramid blending")
-   print("[Input_Options]")
-   print("Path to the input images")
-   print("[Output_Options]")
-   print("Output directory")
-   print("Example usages:")
-   print(sys.argv[0] + " 1 " + "[path to input image] " + "[output directory]")                                # Single input, single output
-   print(sys.argv[0] + " 2 " + "[path to input image1] " + "[path to input image2] " + "[output directory]")   # Two inputs, three outputs
-   print(sys.argv[0] + " 3 " + "[path to input image1] " + "[path to input image2] " + "[output directory]")   # Two inputs, single output
-   
+    print("Usage: [Question_Number] [Input_Options] [Output_Options]")
+    print("[Question Number]")
+    print("1 Histogram equalization")
+    print("2 Frequency domain filtering")
+    print("3 Laplacian pyramid blending")
+    print("[Input_Options]")
+    print("Path to the input images")
+    print("[Output_Options]")
+    print("Output directory")
+    print("Example usages:")
+    print(sys.argv[0] + " 1 " + "[path to input image] " + "[output directory]")  # Single input, single output
+    print(sys.argv[
+              0] + " 2 " + "[path to input image1] " + "[path to input image2] " + "[output directory]")  # Two inputs, three outputs
+    print(sys.argv[
+              0] + " 3 " + "[path to input image1] " + "[path to input image2] " + "[output directory]")  # Two inputs, single output
+
+
 # ===================================================
 # ======== Question 1: Histogram equalization =======
 # ===================================================
+
 def histogram_helper(channel):
 
     maximum_pixel_val = 256
@@ -84,123 +90,146 @@ def histogram_equalization(img_in):
     # cv2.waitKey(0)
 
     return True, img_out
-   
+
+
 def Question1():
+    # Read in input images
+    input_image = cv2.imread(sys.argv[2], cv2.IMREAD_COLOR);
 
-   # Read in input images
-   input_image = cv2.imread(sys.argv[2], cv2.IMREAD_COLOR);
-   
-   # Histogram equalization
-   succeed, output_image = histogram_equalization(input_image)
-   
-   # Write out the result
-   output_name = sys.argv[3] + "1.jpg"
-   cv2.imwrite(output_name, output_image)
+    # Histogram equalization
+    succeed, output_image = histogram_equalization(input_image)
 
-   return True
-   
+    # Write out the result
+    output_name = sys.argv[3] + "1.jpg"
+    cv2.imwrite(output_name, output_image)
+
+    return True
+
+
 # ===================================================
 # ===== Question 2: Frequency domain filtering ======
 # ===================================================
 
 def low_pass_filter(img_in):
-	
-   # Write low pass filter here
-   img_out = img_in # Low pass filter result
-	
-   return True, img_out
+
+    # Write low pass filter here
+    img_out = img_in  # Low pass filter result
+
+    img_in = cv2.cvtColor(img_in, cv2.COLOR_BGR2GRAY)
+    # Take the fft of the image
+    image_fourier_transform = numpy.fft.fft2(img_in)
+    # shift the result by N/2 to bring zero frequency component to center
+    image_fourier_transform_shifted = numpy.fft.fftshift(image_fourier_transform)
+
+    # Create a mask to so that all the low freq pass but high freq are attenuated
+    rows, cols= img_in.shape
+    masklen = 40
+    low_pass_mask = numpy.full_like(image_fourier_transform_shifted, 0)
+    low_pass_mask[rows/2 - masklen: rows/2 + masklen, cols/2 - masklen : cols/2 + masklen ] = 1
+
+    # Apply the low pass filter mask
+    image_fourier_transform_shifted = image_fourier_transform_shifted * low_pass_mask
+
+    # Time to get back the image!
+    # Apply inverse shift
+    inverse_image_fourier_transform = numpy.fft.ifftshift(image_fourier_transform_shifted)
+    img_out = numpy.fft.ifft2(inverse_image_fourier_transform)
+    img_out = numpy.abs(img_out)
+
+    return True, img_out
+
 
 def high_pass_filter(img_in):
+    # Write high pass filter here
+    img_out = img_in  # High pass filter result
 
-   # Write high pass filter here
-   img_out = img_in # High pass filter result
-   
-   return True, img_out
-   
+    return True, img_out
+
+
 def deconvolution(img_in):
-   
-   # Write deconvolution codes here
-   img_out = img_in # Deconvolution result
-   
-   return True, img_out
+    # Write deconvolution codes here
+    img_out = img_in  # Deconvolution result
+
+    return True, img_out
+
 
 def Question2():
+    # Read in input images
+    input_image1 = cv2.imread(sys.argv[2], cv2.IMREAD_COLOR);
+    input_image2 = cv2.imread(sys.argv[3], cv2.IMREAD_COLOR);
 
-   # Read in input images
-   input_image1 = cv2.imread(sys.argv[2], cv2.IMREAD_COLOR);
-   input_image2 = cv2.imread(sys.argv[3], cv2.IMREAD_COLOR);
+    # Low and high pass filter
+    succeed1, output_image1 = low_pass_filter(input_image1)
+    succeed2, output_image2 = high_pass_filter(input_image1)
 
-   # Low and high pass filter
-   succeed1, output_image1 = low_pass_filter(input_image1)
-   succeed2, output_image2 = high_pass_filter(input_image1)
-   
-   # Deconvolution
-   succeed3, output_image3 = deconvolution(input_image2)
-   
-   # Write out the result
-   output_name1 = sys.argv[4] + "2.jpg"
-   output_name2 = sys.argv[4] + "3.jpg"
-   output_name3 = sys.argv[4] + "4.jpg"
-   cv2.imwrite(output_name1, output_image1)
-   cv2.imwrite(output_name2, output_image2)
-   cv2.imwrite(output_name3, output_image3)
-   
-   return True
-   
+    # Deconvolution
+    succeed3, output_image3 = deconvolution(input_image2)
+
+    # Write out the result
+    output_name1 = sys.argv[4] + "2.jpg"
+    output_name2 = sys.argv[4] + "3.jpg"
+    output_name3 = sys.argv[4] + "4.jpg"
+    cv2.imwrite(output_name1, output_image1)
+    cv2.imwrite(output_name2, output_image2)
+    cv2.imwrite(output_name3, output_image3)
+
+    return True
+
+
 # ===================================================
 # ===== Question 3: Laplacian pyramid blending ======
 # ===================================================
 
 def laplacian_pyramid_blending(img_in1, img_in2):
+    # Write laplacian pyramid blending codes here
+    img_out = img_in1  # Blending result
 
-   # Write laplacian pyramid blending codes here
-   img_out = img_in1 # Blending result
-   
-   return True, img_out
+    return True, img_out
+
 
 def Question3():
+    # Read in input images
+    input_image1 = cv2.imread(sys.argv[2], cv2.IMREAD_COLOR);
+    input_image2 = cv2.imread(sys.argv[3], cv2.IMREAD_COLOR);
 
-   # Read in input images
-   input_image1 = cv2.imread(sys.argv[2], cv2.IMREAD_COLOR);
-   input_image2 = cv2.imread(sys.argv[3], cv2.IMREAD_COLOR);
-   
-   # Laplacian pyramid blending
-   succeed, output_image = laplacian_pyramid_blending(input_image1, input_image2)
-   
-   # Write out the result
-   output_name = sys.argv[4] + "5.jpg"
-   cv2.imwrite(output_name, output_image)
-   
-   return True
+    # Laplacian pyramid blending
+    succeed, output_image = laplacian_pyramid_blending(input_image1, input_image2)
+
+    # Write out the result
+    output_name = sys.argv[4] + "5.jpg"
+    cv2.imwrite(output_name, output_image)
+
+    return True
+
 
 if __name__ == '__main__':
-   question_number = -1
-   
-   # Validate the input arguments
-   if (len(sys.argv) < 4):
-      help_message()
-      sys.exit()
-   else:
-      question_number = int(sys.argv[1])
-	  
-      if (question_number == 1 and not(len(sys.argv) == 4)):
-         help_message()
-	 sys.exit()
-      if (question_number == 2 and not(len(sys.argv) == 5)):
-	 help_message()
-         sys.exit()
-      if (question_number == 3 and not(len(sys.argv) == 5)):
-	 help_message()
-         sys.exit()
-      if (question_number > 3 or question_number < 1 or len(sys.argv) > 5):
-	 print("Input parameters out of bound ...")
-         sys.exit()
+    question_number = -1
 
-   function_launch = {
-   1 : Question1,
-   2 : Question2,
-   3 : Question3,
-   }
+    # Validate the input arguments
+    if (len(sys.argv) < 4):
+        help_message()
+        sys.exit()
+    else:
+        question_number = int(sys.argv[1])
 
-   # Call the function
-   function_launch[question_number]()
+        if (question_number == 1 and not (len(sys.argv) == 4)):
+            help_message()
+            sys.exit()
+        if (question_number == 2 and not (len(sys.argv) == 5)):
+            help_message()
+            sys.exit()
+        if (question_number == 3 and not (len(sys.argv) == 5)):
+            help_message()
+            sys.exit()
+        if (question_number > 3 or question_number < 1 or len(sys.argv) > 5):
+            print("Input parameters out of bound ...")
+            sys.exit()
+
+    function_launch = {
+        1: Question1,
+        2: Question2,
+        3: Question3,
+    }
+
+    # Call the function
+    function_launch[question_number]()
